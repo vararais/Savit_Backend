8244a373c59ca95d7a070d3efccdccd12e429c52
vararais <varadigmaisnann@gmail.com>
2026-01-16 23:51:11 +0700

    revisi rute transaction

--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const transactionController = require('../controllers/transactionController');
 
-router.post('/', transactionController.addTransaction);         // Quick Add
-router.get('/history/:stuff_id', transactionController.getHistory); // Lihat Riwayat
+router.post('/', transactionController.addTransaction);
+
+router.get('/:stuff_id', transactionController.getHistory);
+
+router.get('/history/:userId', transactionController.getHistoryByUser);
 
 module.exports = router;
